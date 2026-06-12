1daa683060658b943dcc9afcb40fe6d046f3b249
SevakaGames <sevakagames@Notebook1.localdomain>
2026-06-13 00:18:50 +0300

    fix: forcefully remove old omni makefile

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,14 +1,5 @@
-#
-# Copyright (C) 2026 The Android Open Source Project
-# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_ASUS_I007_1.mk
+    $(LOCAL_PATH)/twrp_ASUS_I007_1.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_ASUS_I007_1-user \
-    omni_ASUS_I007_1-userdebug \
-    omni_ASUS_I007_1-eng
+    twrp_ASUS_I007_1-eng
