e550aff268b7fd65f6f549f9115043fd379038f3
SevakaGames <sevakagames@Notebook1.localdomain>
2026-06-12 23:22:04 +0300

    Initial TWRP tree for ASUS I007D with Virtual AB fixes

---
+++ vendorsetup.sh
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2026 The Android Open Source Project
+# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+add_lunch_combo omni_ASUS_I007_1-user
+add_lunch_combo omni_ASUS_I007_1-userdebug
+add_lunch_combo omni_ASUS_I007_1-eng
